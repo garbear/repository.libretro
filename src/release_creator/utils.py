bef0412e4bc35bf79b197f0afd0ce742c266a9ad
Garrett Brown <garbearucla@gmail.com>
2014-05-19 02:15:40 -0700

    Fix NXEngine core being associated with 3dengine git repository

--- src/release_creator/utils.py
+++ src/release_creator/utils.py
@@ -20,7 +20,24 @@
 import difflib
 import unittest
 
+# I decided to use fuzzy-string matching to associate compiled libretro cores
+# with the project folder in /libretro-super that they came from. In hindsight,
+# this was a poor decision because false detections are too likely. For example,
+# 'nxengine' matches '3dengine' over 'nx'.
+
+# This array hard-codes DLL-to-project  mappings. Problematic matches can be
+# added here, giving a hybrid fuzzy vs. hardcoded matching algorithm. It is
+# evident that a much better system is in need. 
+DLL_TO_PROJECT = {
+    'nxengine': 'nx',
+}
+
+# Use fuzzy string matching to decide which libretro project the compiled DLL
+# came from. This should be renamed to reflect the special cases defined above.
 def getClosestMatch(needle, haystack):
+    if needle in DLL_TO_PROJECT:
+        return DLL_TO_PROJECT[needle]
+    
     matches = difflib.get_close_matches(needle, haystack)
     if not matches:
         return None
@@ -43,9 +60,14 @@ class TestLibretroUtils(unittest.TestCase):
         needle = 'snes9x'
         haystack = ['bsnes', 'snes9x', 'snes9x-next']
         self.assertEqual(getClosestMatch(needle, haystack), haystack[1])
+        
         needle = 'snes9x_next'
         haystack = ['bsnes', 'snes9x', 'snes9x-next']
         self.assertEqual(getClosestMatch(needle, haystack), haystack[2])
+        
+        needle = 'nxengine'
+        haystack = ['nx', '3dengine']
+        self.assertEqual(getClosestMatch(needle, haystack), haystack[0])
 
 if __name__ == '__main__':
     unittest.main()
